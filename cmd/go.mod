6c519917ed9a3891052c4a8a1eaae8aa0496f403
cybershady <zjrundle@gmail.com>
2025-05-08 15:43:22 -0600

    init commit

---
+++ cmd/go.mod
@@ -0,0 +1,3 @@
+module github.com/zachrundle/ocp-installconfig-tui
+
+go 1.23.4
---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    tea "github.com/charmbracelet/bubbletea"
+	"github.com/zachrundle/internal"
+)
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf("zts version %s\n", version.Get())
+	}	
+}
